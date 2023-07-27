11cdc431df0257da5b81721264c4072ae1891904
Trieu Vu <vutrieu2002@gmail.com>
2023-07-27 08:11:35 +0700

    Java Exercise: init project

---
+++ src/main/java/com/example/javaexercise/JavaExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaexercise/JavaExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
